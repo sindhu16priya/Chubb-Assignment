f561e2efd91a96922b02e6c679b7397f467d4a65
sindhu16priya <123161566+sindhu16priya@users.noreply.github.com>
2024-10-31 00:07:23 +0530

    Delete 7_ImageGallery.py

--- 7_ImageGallery.py
+++
@@ -1,9 +0,0 @@
-#7. Image Gallery
-
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/gallery")
-def gallery():
-    return render_template("gallery.html")
-if __name__ == "__main__":
-    app.run(debug=True)
